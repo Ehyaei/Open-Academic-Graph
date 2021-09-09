a42ec7925b73913c0a2172763a4f25b660d90f94
Ahmad <ahmad.ehyaei@gmail.com>
2021-09-09 22:31:26 +0200

    add affiliation chapter

--- manuscript/data-cleansing.R
+++ manuscript/data-cleansing.R
@@ -9,8 +9,12 @@
 library(readr)
 library(stringr)
 library(dplyr)
+library(maps)
+
 
 # extract values from json
+# If anyone wants to read json data from R
+# these functions can be find useful
 
 valueFinder = function(x){
   stringr::str_sub(x, str_locate(x,": ")[,2]+1)
@@ -41,18 +45,62 @@ readMagFiles = function(path){
 ##%######################################################%##
 
 # Affiliations
-rl = readLines("raw-data/affiliations/mag_affiliations.txt")
-cs = read_csv("raw-data/affiliations/mag_affiliations.txt")
 
-affiliation <- readMagFiles(path = "raw-data/affiliations/mag_affiliations.txt")  
-# mutate(Latitude = as.numeric(Latitude), Longitude = as.numeric(Longitude))
+affiliation = read_csv("raw-data/spark/affiliation.csv/part-00000-ad7f2738-e5c6-4744-91b4-c58062714b86-c000.csv")
 
-affiliation %>% filter(Latitude==0)
+hist(affiliation$Latitude,100)
+hist(affiliation$Longitude,100)
+
+# the plot shows there are many location thats wrongly maps to 0
+# we replace it by NA
+affiliation = affiliation %>% 
+  mutate(
+    Latitude = ifelse(Latitude == 0, NA, Latitude),
+    Longitude = ifelse(Longitude == 0, NA, Longitude)
+         )
+
+ind = which(!is.na(affiliation$Longitude) & !is.na(affiliation$Latitude))
+
+affiliation$country = ""
+affiliation$country[ind] <- map.where(database="world", 
+                     affiliation$Longitude[ind], 
+                     affiliation$Latitude[ind])
+
+
+affiliation = affiliation %>% 
+  mutate(country = ifelse(
+    str_detect(country,":"),
+    str_sub(country, 1, str_locate(country,":")[,1]-1),
+    country
+    )
+  )
+    
+library(countrycode)
+affiliation$iso3c = countrycode(affiliation$country, origin = 'country.name', destination = "iso3c")
 write_rds(affiliation,"data/affiliation.rds")
+affiliation %>% select(-NormalizedName,-WikiPage,-url) %>% 
+  write_csv("data/affiliation.csv")
 
-## Venue
-venue <- readMagFiles("raw-data/venue/mag_venues.txt")
+
+# venue
+venue = read_csv("raw-data/spark/venue.csv/part-00000-06643dfd-2de6-4b57-928d-0f58236c4427-c000.csv")
+write_rds(venue,"data/affiliation.rds")
 
 
+############################################################
+#                                                          #
+#                        world Map                         #
+#                                                          #
+############################################################
+
+library(maps)  
+world = map_data("world")  
+world = filter(world,region!="Antarctica")
+world$iso3c = countrycode(world$region, origin = 'country.name', destination = "iso3c")
+world$subregion = NULL
+ggplot(world,aes(x=long,y=lat,group=group))+
+  geom_polygon(color="white",fill = "lightblue")+
+  theme_void()
+write_rds(world,"data/world.rds")
 
 
--- manuscript/global.R
+++ manuscript/global.R
@@ -8,6 +8,28 @@ options(knggitr.kable.NA = '')
 ##%######################################################%##
 library(MPIThemes)
 library(ggplot2)
+library(readr)
+library(readxl)
+library(dplyr)
+library(gt)
+library(kableExtra)
 
 
+############################################################
+#                                                          #
+#                        Map theme                         #
+#                                                          #
+############################################################
 
+map_theme = function(){
+  theme_void()+
+    theme(
+      legend.position="bottom",
+      legend.direction="horizontal",
+      legend.title=element_text(size=rel(.8),vjust = 1,face = "bold"),
+      legend.margin=margin(grid::unit(0,"cm")),
+      legend.text=element_text(size=rel(.7)),
+      legend.key.height=grid::unit(0.25,"cm"),
+      legend.key.width=grid::unit(1.5,"cm")
+    )
+}
